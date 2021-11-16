833fd210b7bd1bcf5e49bd7d368f9987e9ef83a6
Jason Jones <j.jones@ltuc.com>
2021-11-16 13:31:02 +0200

    added back 37 adding 36 will not be possible guys lol

---
+++ class-37/demo/GoodCitizen/app/src/main/java/com/d4coders/goodcitizen/ui/auth/EmailConfirmationActivity.java
@@ -0,0 +1,52 @@
+package com.d4coders.goodcitizen.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.amplifyframework.core.Amplify;
+import com.d4coders.goodcitizen.R;
+import com.d4coders.goodcitizen.ui.auth.fragments.SignUpFragment;
+import com.d4coders.goodcitizen.ui.dashboard.MainActivity;
+
+public class EmailConfirmationActivity extends AppCompatActivity {
+
+    private static final String TAG = "EmailConfirmationActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_confirmation);
+
+        Intent passedData = getIntent();
+        String email = passedData.getStringExtra(SignUpFragment.USER_EMAIL);
+
+        EditText confirmationCode = findViewById(R.id.verification_code_input);
+
+        Button confirmBtn = findViewById(R.id.confirm);
+        confirmBtn.setOnClickListener(view -> {
+            String code = confirmationCode.getText().toString();
+            verifyEmail(code, email);
+        });
+    }
+
+    private void verifyEmail(String verificationCode, String email) {
+        Amplify.Auth.confirmSignUp(
+                email,
+                verificationCode,
+                result -> {
+                    Log.i(TAG,
+                            result.isSignUpComplete() ?
+                                    "Confirm signUp succeeded" :
+                                    "Confirm sign up not complete");
+
+                    startActivity(new Intent(EmailConfirmationActivity.this, MainActivity.class));
+                },
+                error -> Log.e(TAG, error.toString())
+        );
+    }
+}
